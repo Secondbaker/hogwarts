133ba9c0962e5b9367297c5971fb76079fc363f4
secondbaker <secondbaker@gmail.com>
2020-03-09 12:45:05 -0400

    Using route names allows you to automate links

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # GET / => houses#index
   root to: 'houses#index'
   # GET houses/:id => houses#show
-  get 'houses/:id', to: "houses#show", id: /\d+/
+  get 'houses/:id', to: "houses#show", as: 'house', id: /\d+/
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
