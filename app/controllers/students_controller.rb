21d4f07426745d2df76d775b21f7f99ec657f511
secondbaker <secondbaker@gmail.com>
2020-03-10 18:33:40 -0400

    Watch for places where there are extra queries run on the database

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,12 +2,13 @@ class StudentsController < ApplicationController
   
   # GET /students
   def index
-    @students = Student.order(:name)
+    @students = Student.includes(:house).order(:name)
+    #removes n+1 problem with includes
   end
   #immediately tries to load associated view
 
   # GET /students/:id
   def show
-
+    @student = Student.find(params[:id])
   end
 end
