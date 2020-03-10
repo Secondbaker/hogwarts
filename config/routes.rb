2ec1943aafb59904264835dd686f13e30a6270f5
secondbaker <secondbaker@gmail.com>
2020-03-10 11:46:15 -0400

    The student controlled needed two views as well

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
+  get 'students/index'
+  get 'students/show'
   # GET / => houses#index
   root to: 'houses#index'
+  #can have multiple routes to the same endpoint, e.g.:  get 'houses', to: 'houses#index' 
+  #Google may punish this unless you mark one as canonical in metadata
+
   # GET houses/:id => houses#show
-  get 'houses/:id', to: "houses#show", as: 'house', id: /\d+/
+  #get 'houses/:id', to: "houses#show", as: 'house', id: /\d+/
+
+  resources 'houses', only: [:index, :show]
+  #equivalent to GET /houses => houses#index
+  # also GET /houses/:id => houses#show
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
